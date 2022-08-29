ebdddf1b1510acf0f8750fde22e47fb737826814
Evgeni Golov <evgeni@redhat.com>
2022-08-29 09:16:40 +0200

    fix actiongroup branding

--- generate_action_groups.py
+++ generate_action_groups.py
@@ -8,7 +8,7 @@ EXCLUDED_MODULES = ['redhat_manifest']
 with open(META_RUNTIME) as runtime_file:
     runtime = yaml.safe_load(runtime_file)
 
-runtime['action_groups']['foreman'] = sorted(set(ALL_MODULES) - set(EXCLUDED_MODULES))
+runtime['action_groups']['satellite'] = sorted(set(ALL_MODULES) - set(EXCLUDED_MODULES))
 
 with open(META_RUNTIME, 'w') as runtime_file:
     yaml.safe_dump(runtime, runtime_file,  default_flow_style=False, explicit_start=True)
